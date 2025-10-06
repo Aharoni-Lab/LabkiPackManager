08a6d7c4562b34ad31b0aa79efedf9a3a7f1352c
daharoni <dbaharoni@gmail.com>
2025-10-06 14:47:50 -0700

    minor cleaning of special php

--- includes/Special/SpecialLabkiPackManager.php
+++ includes/Special/SpecialLabkiPackManager.php
@@ -1,35 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LabkiPackManager\Special;
 
 use SpecialPage;
 use ExtensionRegistry;
 
-class SpecialLabkiPackManager extends SpecialPage {
+/**
+ * Special page entry point for the LabkiPackManager UI.
+ * Renders a placeholder div and loads the front-end application module.
+ */
+final class SpecialLabkiPackManager extends SpecialPage {
     public function __construct() {
-        parent::__construct( 'LabkiPackManager' );
+        parent::__construct('LabkiPackManager');
     }
 
-    public function getGroupName() {
+    public function getGroupName(): string {
         return 'labki';
     }
 
-    public function getRestriction() {
+    public function getRestriction(): string {
         return 'labkipackmanager-manage';
     }
 
-    public function execute( $subPage ) {
+    public function execute($subPage): void {
         $this->checkPermissions();
 
         $output = $this->getOutput();
-        $output->setPageTitle( $this->msg( 'labkipackmanager-special-title' )->text() );
-		$modules = [ 'ext.LabkiPackManager.styles', 'ext.LabkiPackManager.app' ];
-		if ( class_exists( '\ExtensionRegistry' ) && ExtensionRegistry::getInstance()->isLoaded( 'Mermaid' ) ) {
-			$modules[] = 'ext.mermaid';
-		}
-		$output->addModules( $modules );
-        $output->addHTML( '<div id="labki-pack-manager-root"></div>' );
-	}
+        $output->setPageTitle($this->msg('labkipackmanager-special-title')->text());
+
+        $modules = ['ext.LabkiPackManager.styles', 'ext.LabkiPackManager.app'];
+        if (ExtensionRegistry::getInstance()->isLoaded('Mermaid')) {
+            $modules[] = 'ext.mermaid';
+        }
+
+        $output->addModules($modules);
+        $output->addHTML('<div id="labki-pack-manager-root"></div>');
+    }
 }
-
-
