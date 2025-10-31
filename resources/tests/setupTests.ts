d568951ff17a629bd9df7e9616211026f052112f
daharoni <dbaharoni@gmail.com>
2025-10-31 09:50:17 -0700

    remove old frontend for special page LabkiPackManager

--- resources/tests/setupTests.ts
+++
@@ -1,8 +0,0 @@
-// Minimal globals for tests
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(global as any).mw = {
-  util: { wikiScript: () => '/w/api.php' },
-  config: { get: () => [] }
-};
-
-
